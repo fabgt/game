a3f5a2d06aac63101fb7cfeb572477e4a8e4f0d7
fabgt <fabienclement.desvignes@gmail.com>
2019-10-25 18:24:20 +0200

    training with variables

--- intro.py
+++ intro.py
@@ -1,5 +1,12 @@
 import os
 #os.system("ls -l")#
 
+#print("Math example", 5 / 6 < 1)
+#print("Math example", 50 % 6)
 
-print("Hello world!")
+myVar1 = 12
+myVar2 = 10
+newVar = myVar1 > myVar2
+print("result:",newVar)
+car_brand = 'volvo'
+print(car_brand)
