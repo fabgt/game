1b1c9e247da38e9cde057c7d4b935bee0497938c
fabgt <fabienclement.desvignes@gmail.com>
2019-10-27 17:07:19 +0100

    copy from one file to another

--- textTraining.py
+++ textTraining.py
@@ -1,34 +1,23 @@
 from sys import argv
+from os.path import exists
 
-script, filename = argv
+script, from_file, to_file = argv
 
-txt = open(filename)
+print(f"Copying from {from_file} to {to_file}")
 
-print(f"We are going to erase {filename}.")
-print("If you don't want that, hit CTRL+C (^C).")
-print("If you do want that, hit RETURN.")
+#we could do these two on one line, how?
+in_file = open(from_file)
+indata = in_file.read()
 
-input("?")
+print(f"The input file is {len(indata)} bytes long")
 
-print("Opening the file...")
-target=open(filename,'w')
+print(f"Does the output file exist? {exists(to_file)}")
+print("Ready, hit Return to continue, CTRL+C to abort.")
 
-print("Truncating the file. Goodbye!")
-target.truncate()
+out_file = open(to_file, 'w')
+out_file.write(indata*2)
 
-print("Now I'm going to ask you for three line.")
-line1=input("line 1: ")
-line2=input("line 2: ")
-line3=input("line 3: ")
+print("Alright, all done.")
 
-print("I'm going to write these to the file.")
-
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
-
-print("And finally, we close it.")
-target.close()
+out_file.close()
+in_file.close()
