9d15c7ed6a07615fe01085751063e1cee0025b38
fabgt <fabienclement.desvignes@gmail.com>
2020-06-05 11:10:00 +0200

    new file to train

---
+++ python_training.py
@@ -0,0 +1,28 @@
+import os
+from sys import argv
+
+script, first_Arg, sec_Arg = argv
+
+formatter = "{} {}"
+print(formatter.format(
+        "Try",
+        "****ker"))
+#_______________
+#Jumping lines
+tryVar = "Jan\nFeb\nMarch"
+print(tryVar)
+print("""
+Here is some text
+""")
+#______________
+#Asking QUESTION:
+print("What is your name?", end=' ')
+name=input()
+print(f"Hello {name}")
+age=input("how old are you? ")
+print(age)
+#____
+print(f"First argument is: {first_Arg}, second argument is: {sec_Arg}")
+prompt="> "
+height=input("""How tall are you?
+>""")
